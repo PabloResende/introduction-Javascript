1ed00e5e39b3a6d3c5b5693aee06c81abac120a7
pablo <wademcfild@gmail.com>
2023-07-17 10:07:28 -0300

    ...

---
+++ 5alert_confirm_prompt/js/script.js
@@ -0,0 +1,10 @@
+alert('hello world');
+// aparece um caixa no navegador mostrando a mensagem
+
+window.confirm('deseja realmente apagar?')
+// cria uma janela de confirmação no navegador
+// cancelar retorna false
+// ok retorna true
+
+window.prompt('digite seu nome')
+// retorna uma caixa para 
---
+++ 5alert_confirm_prompt/js/soma_variaveis.js
@@ -0,0 +1,11 @@
+let num1 = window.prompt('digite um número');
+let num2 = window.prompt('digite outro número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resulado = num1 + num2;
+alert('o resultado da sua conta foi ' + resulado)
+alert(`o resultado foi ${num1 +num2}`)
+alert(`o resultado foi ${resulado}`)
+// funciona das 3 formas

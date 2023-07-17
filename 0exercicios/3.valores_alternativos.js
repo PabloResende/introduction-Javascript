cfa1aec0af38b3cd7b3fd0a8308c9a040763a6a3
pablo <wademcfild@gmail.com>
2023-07-17 10:24:06 -0300

    ...

---
+++ 0exercicios/3.valores_alternativos.js
@@ -0,0 +1,14 @@
+let varA = 'a';
+let varB = 'b';
+let varC = 'c';
+
+//o desafio é fazer as variáveis aparecerem na seguinte ordem: b, c, a
+//porém não pode apenas trocar o valor de varA por b diretamente;
+
+const varAtemp = varA;
+
+varA = varB;
+varB = varC;
+varC = varAtemp;
+
+console.log(varA, varB, varC);

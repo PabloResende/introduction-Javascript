10d0516f62421542c284ff0da17ddfe7d5070603
pablo <wademcfild@gmail.com>
2023-07-20 15:39:02 -0300

    ...

---
+++ 6.mais numeros/formatar_numero.js
@@ -0,0 +1,7 @@
+// para formatar um número é em tantas casas decimais se usa o método toFixed:
+
+let num1 = 10.3124214;
+
+console.log(num1.toFixed(3)); // 10.312
+
+//o 3 dita as casas decimais depois do ponto

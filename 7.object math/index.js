d796714d7ba2878abb9eacdb4e56317382482628
pablo <wademcfild@gmail.com>
2023-07-20 15:57:55 -0300

    ...

---
+++ 7.object math/index.js
@@ -0,0 +1,15 @@
+let num1 = 9.49;
+let num2 = Math.floor(num1); // arredonda pra baixo
+
+let num3 = Math.ceil(num1); // arredonda pra cima
+
+let num4 = Math.round(num1); // arredonda para o mais próximo
+
+console.log(Math.max(1, 3, 5, -1500, 1, 5, 20, -50)); // max pega o maior número
+
+console.log(Math.min(1, 3, 5, -1500, 1, 5, 20, -50)); // min pega o menor número
+
+console.log(Math.random(1, 3, 5, -1500, 1, 5, 20, -50)); // random pega qualquer número da lista
+
+console.log(Math.pow(2, 10)); //potenciação, no caso 2 elevado a 10
+console.log(2 ** 10); //outra forma de fazer potenciação

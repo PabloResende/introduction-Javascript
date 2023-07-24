fcd311a04d3c3f1361cf9b52d1acb9117edf28e6
pablo <wademcfild@gmail.com>
2023-07-24 15:05:44 -0300

    ...

--- javascript-basic/8.arrays-basic/index.js
+++ javascript-basic/8.arrays-basic/index.js
@@ -1,7 +1,7 @@
             //012345678910
 const nome = 'Pablo Silva'
 
-                //0        1    2    3    4   
+                // 0      1    2    3    4   
 const alunos = ['luiz', 'Anna', 1, true, null]
 //não é uma boa prática de programação misturar vários elementos
 //em um único array
@@ -9,4 +9,4 @@ const alunos = ['luiz', 'Anna', 1, true, null]
 console.log(alunos);
 //para acessar um índice específico do array basta pro colchetes com o índice respectivo
 console.log(alunos[1]) // resultado 'Anna' (sem as aspas)
-
+console.log(alunos.length); //contará quantos índices tem no array

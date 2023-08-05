e6a20f482e994aee19700bd22b835f9d286ea634
Sr.Carrilho <126430073+WadeMcfild@users.noreply.github.com>
2023-08-05 16:01:58 -0300

    ...

--- 5funcoes/index.js
+++ 5funcoes/index.js
@@ -9,5 +9,17 @@ function soma (x, y) { //para criar uma função é usado advinha só? function
     return resultado; //então é retornado a 'ação' que foi feita com os parâmetros que nesse caso foi a soma
 }
 
-const resultado = soma(2, 2); //aqui é onde os valores dos parâmetros é definido
+const resultado = soma(2, 2); //aqui é onde os argumentos dos parâmetros são definidos
 console.log(resultado) // aqui é para visualizar a constante que atribuiu valor aos parâmetros
+
+// --------------------------------------------- // ------------------------------------------------------ //
+
+//outra forma de fazer funções:
+
+const raiz = function (n) {
+    return n ** 0.5 //ou 1/2
+}; //precisa terminar com ; nesse jeito de fazer
+
+console.log(raiz(9));
+console.log(raiz(25));
+console.log(raiz(65));

e220ef96a80afcda1c3002fb0a8e54919e5f779c
pablo <wademcfild@gmail.com>
2023-08-29 15:00:52 -0300

    ...

--- logica/if-else/index.js
+++ logica/if-else/index.js
@@ -17,3 +17,15 @@ if (hora >= 0 && hora <= 11) {
     console.log('olá');
 }
 
+// para deixar o código mais limpo e diminuir a quantidade de if e elses, pode ser usado um operador condicional ternário:
+
+const hour = 10;
+const message =
+  hour >= 0 && hour <= 11 ? 'bom dia' :
+  hour >= 12 && hour <= 17 ? 'boa tarde' :
+  hour >= 18 && hour <= 23 ? 'boa noite' :
+  'olá';
+
+console.log(message);
+
+//Obs: faça os códigos sempre em inglês!

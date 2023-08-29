7ead0fd23c939faab920851c0cc6fcaea3ba3565
pablo <wademcfild@gmail.com>
2023-08-29 10:45:39 -0300

    ...

---
+++ logica/operadores logicos/index.js
@@ -0,0 +1,13 @@
+/* 
+
+&& == AND
+|| == OR
+! == NOT
+
+*/
+
+console.log(true && true && true); //retorna verdadeiro pois ambos são verdadeiros, se algum parâmetro for falso, toda a expressão retorna false;
+
+console.log(true || false || false); //no caso do or, se qualquer um dos parâmetros for verdadeiro, independente dos outros, toda axpressão retorna true;
+
+console.log()

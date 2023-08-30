d7326c797286d8b4679eaf1f88a85eb2abaeee2c
pablo <wademcfild@gmail.com>
2023-08-30 10:53:01 -0300

    ...

---
+++ logica/12.operador_ternario/index.js
@@ -0,0 +1,17 @@
+const pontuacaoUsuario = 999;
+
+//if else normal:
+
+if (pontoUsuario >= 1000) {
+    console.log('Usuário VIP');
+} else {
+    console.log('Usuário normal');
+}
+
+//operador ternário:
+// (condicao) ? true : false;
+
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'usuário VIP' : 'Usuário normal';
+
+const corUsuario = 'Pink';
+const corPadrao = corUsuario || 'Preta'

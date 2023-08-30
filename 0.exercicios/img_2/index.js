5932a694d9dc740855c97347b10def2a27be241b
pablo <wademcfild@gmail.com>
2023-08-30 09:43:55 -0300

    ....

--- 0.exercicios/img_2/index.js
+++ 0.exercicios/img_2/index.js
@@ -1,6 +1,28 @@
+// Seleciona o elemento HTML com o ID 'formulario' e guarda-o na variável 'form'
 const form = document.querySelector('#formulario');
 
 form.addEventListener('submit', function (event) {
+    // Adiciona um ouvinte de evento para o evento 'submit' no formulário
     event.preventDefault();
+    // Previne o comportamento padrão de envio do formulário, que seria atualizar a página
     console.log('envento previnido')
+    // Exibe uma mensagem no console para indicar que o evento foi prevenido
+    setResultado('olá mundo!')
+    // Chama a função 'setResultado' passando a mensagem 'olá mundo!'
 });
+
+// Declaração da função 'setResultado', que recebe uma mensagem como parâmetro
+function setResultado (msg) {
+    const resultado = document.querySelector('#resultado');
+    // Seleciona o elemento HTML com o ID 'resultado' e guarda-o na variável 'resultado'
+    resultado.innerHTML = '';
+    // Limpa o conteúdo dentro do elemento 'resultado'
+    const p = document.createElement('p');
+    // Cria um novo elemento HTML do tipo 'p' (parágrafo)
+    p.classList.add('paragrafo-resultado');
+    // Adiciona a classe 'paragrafo-resultado' ao elemento 'p'
+    p.innerHTML = 'Qualquer coisa';
+    // Define o conteúdo interno do elemento 'p' como 'Qualquer coisa'
+    resultado.appendChild(p);
+    // Adiciona o elemento 'p' como filho do elemento 'resultado'
+}

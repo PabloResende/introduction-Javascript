bb9b8ad97c7b8e65ed097e42f78ecd67bc4f7411
pablo <wademcfild@gmail.com>
2023-08-30 15:06:03 -0300

    ...

--- logica/14.switch/index.js
+++ logica/14.switch/index.js
@@ -1,4 +1,48 @@
-const data = new Date('1987-04-20 00:00:00');
+const data = new Date("1987-04-20 00:00:00");
 const diaSemana = data.getDay();
 
-console.log(diaSemana);
+switch (diaSemana) {
+  case 0:
+    diaSemanaTexto = "domingo";
+    break;
+  case 1:
+    diaSemanaTexto = "segunda";
+    break;
+  case 2:
+    diaSemanaTexto = "terça";
+    break;
+  case 3:
+    diaSemanaTexto = "quarta";
+    break;
+  case 4:
+    diaSemanaTexto = "quinta";
+    break;
+  case 5:
+    diaSemanaTexto = "sexta";
+    break;
+  case 6:
+    diaSemanaTexto = "sábado";
+    break;
+}
+
+// é igual if e else:
+
+console.log(diaSemanaTexto);
+
+if (diaSemana === 0) {
+  diaSemanaTexto = "domingo";
+} else if (diaSemana === 1) {
+  diaSemanaTexto = "segunda";
+} else if (diaSemana === 2) {
+  diaSemanaTexto = "terça";
+} else if (diaSemana === 3) {
+  diaSemanaTexto = "quarta";
+} else if (diaSemana === 4) {
+  diaSemanaTexto = "quinta";
+} else if (diaSemana === 5) {
+  diaSemanaTexto = "sexta";
+} else if (diaSemana === 6) {
+  diaSemanaTexto = "sábado";
+}
+
+console.log(diaSemanaTexto);

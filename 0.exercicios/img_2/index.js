77e01e87027edfff932dbeb9e987201266ab3bb8
pablo <wademcfild@gmail.com>
2023-08-30 09:56:53 -0300

    ...

--- 0.exercicios/img_2/index.js
+++ 0.exercicios/img_2/index.js
@@ -5,11 +5,29 @@ form.addEventListener('submit', function (event) {
     // Adiciona um ouvinte de evento para o evento 'submit' no formulário
     event.preventDefault();
     // Previne o comportamento padrão de envio do formulário, que seria atualizar a página
-    console.log('envento previnido')
-    // Exibe uma mensagem no console para indicar que o evento foi prevenido
-    setResultado('olá mundo!')
-    // Chama a função 'setResultado' passando a mensagem 'olá mundo!'
+    const inputPeso = event.target.querySelector('#peso');
+    const inputAltura = event.target.querySelector('#altura');
+    // Pego todo o input de altura e peso
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+    // Pega o valor das inputs peso e altura
+
+    if (!peso) {
+        setResultado('Peso inválido', false);
+        return;
+    }
+
+    if (!altura) {
+        setResultado('Altura inválida');
+        return;
+    }
 });
+
+function criarP () {
+    const p = document.createElement('p');
+    return p;
+}
 
 // Declaração da função 'setResultado', que recebe uma mensagem como parâmetro
 function setResultado (msg) {
@@ -17,12 +35,5 @@ function setResultado (msg) {
     // Seleciona o elemento HTML com o ID 'resultado' e guarda-o na variável 'resultado'
     resultado.innerHTML = '';
     // Limpa o conteúdo dentro do elemento 'resultado'
-    const p = document.createElement('p');
-    // Cria um novo elemento HTML do tipo 'p' (parágrafo)
-    p.classList.add('paragrafo-resultado');
-    // Adiciona a classe 'paragrafo-resultado' ao elemento 'p'
-    p.innerHTML = 'Qualquer coisa';
-    // Define o conteúdo interno do elemento 'p' como 'Qualquer coisa'
-    resultado.appendChild(p);
-    // Adiciona o elemento 'p' como filho do elemento 'resultado'
+    const p = criarP();
 }

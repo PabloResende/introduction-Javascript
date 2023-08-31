3d0e3bb742d3bd3ba0727204d1bfe3330801a619
pablo <wademcfild@gmail.com>
2023-08-31 11:17:01 -0300

    ...

--- logica/11.operadores_logicos/index.js
+++ logica/11.operadores_logicos/index.js
@@ -6,8 +6,8 @@
 
 */
 
-console.log(true && true && true); //retorna verdadeiro pois ambos são verdadeiros, se algum parâmetro for falso, toda a expressão retorna false;
+console.log(true && true && true); // retorna verdadeiro pois ambos são verdadeiros, se algum parâmetro for falso, toda a expressão retorna false;
 
-console.log(true || false || false); //no caso do or, se qualquer um dos parâmetros for verdadeiro, independente dos outros, toda axpressão retorna true;
+console.log(true || false || false); // no caso do or, se qualquer um dos parâmetros for verdadeiro, independente dos outros, toda axpressão retorna true;
 
 console.log()

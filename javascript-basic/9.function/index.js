61d8dbc7db8cac8a8bb099ca3cea72ff2cdfbf4b
pablo <wademcfild@gmail.com>
2023-08-31 10:42:06 -0300

    ...

--- javascript-basic/9.function/index.js
+++ javascript-basic/9.function/index.js
@@ -20,6 +20,23 @@ function saudacao(nome) {
     return `Bom dia ${nome}`;
 } // a funcção irá retornar 'undefined' caso não seja usado o 'return'
 // sempre use um retorne e não um console.log por exemplo, é uma boa prática
+// nada é executado depois de return
 
 const variavel = saudacao('Luiz');
 console.log(variavel) // 'Bom dia Luiz'
+
+function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
+}
+
+console.log(soma(2, 2)); // 4
+console.log(soma(34, 10)); // 44
+console.log(soma(406, 38)); // 444
+
+function nomeSobrenome(nome, sobrenome) {
+    const saudacao = `bem vindo ${nome} ${sobrenome}`
+    return saudacao;
+}
+
+console.log(nomeSobrenome('pablo', 'silva')); // é útil separar dessa forma por vírgula quando for string, caso contrário irá aparecer um undefined após a execução da função

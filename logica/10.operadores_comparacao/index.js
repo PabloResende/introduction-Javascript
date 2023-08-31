8a22d708d900156d220ca914b25fe4babdbb3d4d
pablo <wademcfild@gmail.com>
2023-08-31 11:16:23 -0300

    ...

--- logica/10.operadores_comparacao/index.js
+++ logica/10.operadores_comparacao/index.js
@@ -11,14 +11,14 @@
 
 */
 
-console.log(10 > 5); //true, 10 é maior que 5
+console.log(10 > 5); // true, 10 é maior que 5
 
 const expressao = 10 >= 5;
-console.log(expressao) //nesse caso, sendo igual ou maior já consta como true
+console.log(expressao) // nesse caso, sendo igual ou maior já consta como true
 
-console.log(10 === 10); //true, tanto o valor quanto o tipo são iguais;
-console.log(10 === '10') //false, pois o valor é igual mas o tipo é diferente
-console.log(10 == '10'); //consta como true mesmo sendo de tipos diferentes
+console.log(10 === 10); // true, tanto o valor quanto o tipo são iguais;
+console.log(10 === '10') // false, pois o valor é igual mas o tipo é diferente
+console.log(10 == '10'); // consta como true mesmo sendo de tipos diferentes
 
-console.log(10 != '10'); //false, mesmo sendo tipos diferentes aponta como iguais, por isso não é recomendado
-console.log(10 !== '10'); //true, aqui é o contrário, aponta como diferentes pois são tipos diferentes
+console.log(10 != '10'); // false, mesmo sendo tipos diferentes aponta como iguais, por isso não é recomendado
+console.log(10 !== '10'); // true, aqui é o contrário, aponta como diferentes pois são tipos diferentes

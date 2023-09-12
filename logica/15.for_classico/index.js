61e9decc030a46dcf89d34aaa26c54cba93c90f3
pablo <wademcfild@gmail.com>
2023-09-12 08:50:55 -0300

    ...

---
+++ logica/15.for_classico/index.js
@@ -0,0 +1,30 @@
+// jeito noob de fazer:
+
+// console.log('linha 0')
+// console.log('linha 1')
+// console.log('linha 2')
+// console.log('linha 3')
+// console.log('linha 4')
+// console.log('linha 5')
+
+// i = index
+
+
+
+for(let i = 0; i <= 5; i++) {
+    console.log(`linha ${i}`)
+    // enquanto i for menor ou igual á 5 adicina mais 1
+} 
+/*
+ E então a iteração é adicionado ao final de 'linha'
+agora ao invés de usar 6 console.log
+se usa uma simples estrutura de repetição:
+    linha 0
+    linha 1
+    linha 2
+    linha 3
+    linha 4
+    linha 5
+*/
+
+

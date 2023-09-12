9578848eef085e2e2612d1f126e3938543b6075a
pablo <wademcfild@gmail.com>
2023-09-12 16:50:09 -0300

    ...

--- 0.exercicios/8.fizzBuzz.js
+++ 0.exercicios/8.fizzBuzz.js
@@ -2,10 +2,11 @@
 // número é divisível por 3 = fizz
 // número é divisível por 5 = buzz
 // número é divisível por 2 e por 5 = FizzBuzz
-// se der false retorna o próprio número
+// Número não é divisível por 3 nem por 5 retorna o próprio número
+// use a função com números de 10 a 100
 
-const Fizz = (x) => x % 3 === 0 ? 'Fizz' : x;
-const Buzz = (y) => y % 5 === 0 ? 'Buzz' : y;
-const FizzBuzz = (z) => z % 5 && 3 === 0 ? 'FizzBuzz' : z;
+const FizzBuzz = (n) => n % 3 === 0 && n % 5 === 0 ? 'FizzBuzz' : n && n % 5 === 0 ? 'Buzz' : n && n % 3 === 0 ? 'Fizz' : n;
 
-console.log(Fizz(9), Buzz(10), FizzBuzz(9))
+for (let i = 0; i <= 100; i ++) {
+    console.log( i, FizzBuzz(i))
+}

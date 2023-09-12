2bdd4ecaed8e718dc5ba72887b9c46f00887e9be
pablo <wademcfild@gmail.com>
2023-09-12 15:35:05 -0300

    ...

---
+++ logica/21.refatorando_funcao/index.js
@@ -0,0 +1,36 @@
+// o objetivo é refatorar uma função que recebe dois números e retorna o maior:
+
+
+// estagiário //
+
+function max(x, y) {
+  if (x > y) return x;
+  else {
+    return y;
+  }
+}
+
+const maior = max(10, 20);
+console.log(maior);
+
+// júnior //
+
+function max2(x, y) {
+  if (x > y) return x;
+  return y;
+}
+
+console.log(max2(30, 20));
+
+// pleno //
+
+function max3(x, y) {
+    return x > y ? x : y;
+  }
+  
+  console.log(max3(10, 15));
+  
+  // senior //
+  
+  const max4 = (x, y) => x > y ? x : y;
+  console.log(max4(24, 42));

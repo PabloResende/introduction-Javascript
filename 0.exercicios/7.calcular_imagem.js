87afcd9d01a92422f05a706152188265fa5b473c
pablo <wademcfild@gmail.com>
2023-09-12 15:56:28 -0300

    ...

--- 0.exercicios/7.calcular_imagem.js
+++ 0.exercicios/7.calcular_imagem.js
@@ -1,17 +1,20 @@
 // jeito foda de fazer
 
-const ePaisagem = (x, y) => x === 1608 && y === 408 ? 'está em modo paisagem' : 'não está em modo paisagem';
-console.log(ePaisagem(1608, 408))
+const ePaisagem = (x, y) =>
+  x === 1608 && y === 408
+    ? "está em modo paisagem"
+    : "não está em modo paisagem";
+console.log(ePaisagem(1608, 408));
 
 // jeito nelson
 
 function ePaisagemNelson(x, y) {
-    if (x === 1608 && y === 408) {
-        return 'está em modo paisagem'
-    } else {
-        return 'não está em modo paisagem'
-    }
+  if (x === 1608 && y === 408) {
+    return "está em modo paisagem";
+  } else {
+    return "não está em modo paisagem";
+  }
 }
 
-const confere = ePaisagemNelson(1366, 768)
-console.log(confere)
+const confere = ePaisagemNelson(1366, 768);
+console.log(confere);

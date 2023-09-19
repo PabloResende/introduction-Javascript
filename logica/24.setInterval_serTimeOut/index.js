3f2b938133f04dc15a1eadacb199385509be6053
pablo <wademcfild@gmail.com>
2023-09-19 15:02:35 -0300

    ...

--- logica/24.setInterval_serTimeOut/index.js
+++ logica/24.setInterval_serTimeOut/index.js
@@ -7,3 +7,25 @@ function mostraHora() {
 }
 
 console.log(mostraHora()); // função básica que retorna hora atual
+
+const timer = setInterval (function() { 
+    console.log(mostraHora());
+}, 1000) // cria uma função anonima que tem a tarefa de executar 'mostraHora' a cada 1000s
+
+setTimeout(function() {
+    clearInterval(timer)
+}, 5000);
+
+// diferente de setInterval, setTimout só é executado uma vez após chegar no tempo estipulado
+
+setTimeout(() => {
+    console.log('olá')
+}, 1000); // como o tempo desse setTimeout é menor que o de cima ele é executado durante:
+
+// 15:00:17
+// 15:00:18
+// olá
+// 15:00:19
+// 15:00:20
+// 15:00:21
+

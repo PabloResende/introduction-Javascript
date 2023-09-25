27080f4b6b421b7972c32d4b9f33b6bd07bb024e
pablo <wademcfild@gmail.com>
2023-09-25 09:14:09 -0300

    ...

--- funcoes(avancado)/26.parametros/index.js
+++ funcoes(avancado)/26.parametros/index.js
@@ -1,13 +1,29 @@
-function funcao(a, b =2 ) { // forma melhor de atribuir um valor padrão ao B se por acaso ele não ganhar um mais a frente
-    console.log(a + b)
+function funcao(a, b = 2) {
+  // forma melhor de atribuir um valor padrão ao B se por acaso ele não ganhar um mais a frente
+  console.log(a + b);
 }
 
-funcao(2) // somente o valor de A está sendo passado, portanto B irá assumir seu valor padrão
+funcao(2); // somente o valor de A está sendo passado, portanto B irá assumir seu valor padrão
 
-function conta(operador, acumulador, ...numeros){ 
-    // os três pontinhos antes de 'numero' dizem que após os índices de 'operador' e de 'acumulador' todo o resto pertence á 'numeros'
-    console.log(operador, acumulador, numeros)
+function conta(operador, acumulador, ...numeros) {
+  // os três pontinhos antes de 'numero' dizem que após os índices de 'operador' e de 'acumulador' todo o resto pertence á 'numeros'
+  for (let numero of numeros) // com 'of' ele pega os numeros de 'numero' e com 'in' pega os índices
+    if (operador === "+") {
+      acumulador += numeros;
+    }
+  if (operador === "-") {
+    acumulador -= numeros;
+  }
+  if (operador === "/") {
+    acumulador /= numeros;
+  }
+  if (operador === "*") {
+    acumulador *= numeros;
+  }
+
+    console.log(numero);
 }
 
-conta('+', 0, 20, 30, 40, 50); // dessa forma tudo depois de 0 pertence á 'numeros'
-conta('+', 0, [10, 20, 30, 40]) // dessa forma também porém assim não precisa dos três pontinhos
+conta("+", 0, 20, 30, 40, 50); // dessa forma tudo depois de 0 pertence á 'numeros'
+conta("+", 0, [10, 20, 30, 40]); // dessa forma também porém assim não precisa dos três pontinhos
+

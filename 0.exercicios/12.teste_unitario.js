13aad74f50bc3617f53294524d282187a3d93dfd
pablo <wademcfild@gmail.com>
2023-09-25 15:19:58 -0300

    ...

---
+++ 0.exercicios/12.teste_unitario.js
@@ -0,0 +1,18 @@
+const error = "número, operador ou acumulador inválido";
+
+const calculo = (operador, acumulador, numeros) => {
+  for (let numero of numeros) {
+    operador === "+"
+      ? (acumulador += numero)
+      : operador === "*"
+      ? (acumulador *= numero)
+      : operador === "/"
+      ? (acumulador /= numero)
+      : operador === "-"
+      ? (acumulador -= numero)
+      : error;
+    console.log(acumulador);
+  }
+};
+
+calculo("+", 1, [20, 30, 40, 50]);

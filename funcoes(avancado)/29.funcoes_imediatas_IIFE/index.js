e4ef8851724b8d521b3a96da6d8dcdfdcb9e5059
pablo <wademcfild@gmail.com>
2023-09-26 10:00:22 -0300

    ....

--- funcoes(avancado)/29.funcoes_imediatas_IIFE/index.js
+++ funcoes(avancado)/29.funcoes_imediatas_IIFE/index.js
@@ -4,15 +4,17 @@
 // tudo dentro dela é isolado, mesmo nomes de variáveis iguais não são dadas como erradas:
 
 (function(){
-    const nome = 'Pablo'
+    const nome = 'Pedro'
     console.log(nome)
 })(); // se precisar passar argumentos a algum parâmetro pode se usar esse () no final...
 // está função vai ser executada automaticamente
 // útil em situações extritamente simples, ou se por acaso tiver que criar todo um sistema isolado
 
-const nome = 'Pedro' // mesmo já tendo uma variável com este nome não dá erro, pois o escopo da função de cima é fechado diferente do de baixo que é global
+const nome = 'Jão'; // mesmo já tendo uma variável com este nome não dá erro, pois o escopo da função de cima é fechado diferente do de baixo que é global
+console.log(nome);
 
 (function (idade, peso, altura){
     const nome = 'Pablo'
     const sobrenome = 'silva'
-})
+    console.log(`${nome} ${sobrenome}, tem ${idade} anos, ${peso}kg e ${altura} de altura`)
+}) (20, 58, 1.65);

92658f9dd8966c91148139c0e5d487585d1f2a04
pablo <wademcfild@gmail.com>
2023-10-04 11:28:15 -0300

    ...

---
+++ objetos_prototypes(avancado)/39.object.defineProperty()_object.defineProperties()/index.js
@@ -0,0 +1,10 @@
+// Object.defineProperty() - Object.defineProperties()
+
+function Produto(nome, preco, estoque) { // constructor function
+    this.nome,
+    this.preco,
+    this.estoque
+}
+
+const produto1 = new Produto('camisa', 20, 3)
+console.log(produto1)

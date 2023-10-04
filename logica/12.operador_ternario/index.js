ab84e0d60a80a541feb68de1be09c7a77cd50a39
pablo <wademcfild@gmail.com>
2023-10-04 08:23:59 -0300

    ...

--- logica/12.operador_ternario/index.js
+++ logica/12.operador_ternario/index.js
@@ -3,17 +3,21 @@ const pontuacaoUsuario = 999;
 // if else normal:
 
 if (pontoUsuario >= 1000) {
-    console.log('Usuário VIP');
+  console.log("Usuário VIP");
 } else {
-    console.log('Usuário normal');
+  console.log("Usuário normal");
 }
 
 // operador ternário:
 // (condicao) ? true : false;
 
-const nivelUsuario = pontuacaoUsuario >= 1000 ? 'usuário VIP' : 'Usuário normal';
+const nivelUsuario =
+  pontuacaoUsuario >= 1000 ? "usuário VIP" : "Usuário normal";
 
 // outro jeito:
 
-const corUsuario = 'Pink';
-const corPadrao = corUsuario || 'Preta'
+const corUsuario = "Pink";
+const corPadrao = corUsuario || "Preta";
+
+// é importante entender que apesar de  muito prática, pode ser 'perigoso' usa-l0
+// em caso de if/else simples, é ótimo, porém para um lógica muito longa fica horrível de ler, e pode dar muita dor de cabeça para adicionar novas condições, isto porque muito provavelmente terá que reescrever boa parte do ternário

64295a6aaa4f0d4442c878f17d1c669800d8e43a
pablo <wademcfild@gmail.com>
2023-10-04 08:11:21 -0300

    ....

--- javascript-basic/8.arrays_basic/index.js
+++ javascript-basic/8.arrays_basic/index.js
@@ -5,7 +5,7 @@ const alunos = ['luiz', 'Anna', 1, true, null]
 // não é uma boa prática de programação misturar vários elementos
 // em um único array
 // para acessar um índice específico do array basta pôr colchetes com o índice respectivo
-console.log(alunos[1]) // resultado 'Anna' (sem as aspas)
+console.log(alunos[1]) // Anna
 console.log(alunos.length); // contará quantos índices tem no array, que é 5
 alunos[alunos.length] = 'Luiza' // além de contar também irá adicionar 'Luiza' ao array
 console.log(alunos);
@@ -16,8 +16,6 @@ console.log(alunos.length) //7
 alunos.unshift('Lacerda') // adiciona no início da array
 alunos[1] = 'Pablo'
 console.log(alunos)
-console.log((alunos[1]),'ama muito', (alunos[2]),(alunos[6]),(alunos[7]),(alunos[0]),'!!')
-// Pablo ama muito Anna Luíza Estácio Lacerda
 alunos.pop()// remove o último
 // é possível armazenar o elemento removido em uma constante
 // e então chamá lo de acordo com o nome dado a constante atrelado ao mesmo
@@ -32,5 +30,5 @@ console.log(typeof alunos) //object
 // retorna objeto, e não array
 // em java um array é um objeto
 console.log(alunos instanceof Array) //True
-// essa é uma forma de descobrir se você estpa usando array
+// essa é uma forma de descobrir se você esta usando array
 // se for True é um array

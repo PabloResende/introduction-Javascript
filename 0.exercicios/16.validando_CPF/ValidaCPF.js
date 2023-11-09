7c213832c50195033078c14b99078b7bbf26cb58
pablo <wademcfild@gmail.com>
2023-11-09 08:26:11 -0300

    ...

--- 0.exercicios/16.validando_CPF/ValidaCPF.js
+++ 0.exercicios/16.validando_CPF/ValidaCPF.js
@@ -14,51 +14,22 @@ Se o número digito for maior que 9, consideramos 0.
 11 - (284 % 11) = 2 (Primeiro dígito)
 Se o número digito for maior que 9, consideramos 0.
 */
-function ValidaCPF(cpfEnviado) {
-  Object.defineProperty(this, 'cpfLimpo', {
-    enumerable: true,
-    get: function() {
-      return cpfEnviado.replace(/\D+/g, '');
-    }
+
+function validaCPF(cpfEnviado) {
+  Object.defineProperty(this, "cpfLimpo", {
+    get: function () {
+      return cpfEnviado.replace(/\D+/g, "");
+    },
   });
 }
 
-ValidaCPF.prototype.valida = function() {
-  if(typeof this.cpfLimpo === 'undefined') return false;
-  if(this.cpfLimpo.length !== 11) return false;
-  if(this.isSequencia()) return false;
-
-  const cpfParcial = this.cpfLimpo.slice(0, -2);
-  const digito1 = this.criaDigito(cpfParcial);
-  const digito2 = this.criaDigito(cpfParcial + digito1);
-
-  const novoCpf = cpfParcial + digito1 + digito2;
-  return novoCpf === this.cpfLimpo;
+validaCPF.prototype.valida = function () {
+  if (typeof this.cpfLimpo === "undefined") return false;
+  if(this.cpfLimpo.length !== 11) false
+  return true;
 };
 
-ValidaCPF.prototype.criaDigito = function(cpfParcial) {
-  const cpfArray = Array.from(cpfParcial);
+const cpf = new validaCPF("705.484.450-52");
+cpf.valida();
 
-  let regressivo = cpfArray.length + 1;
-  const total = cpfArray.reduce((ac, val) => {
-    ac += (regressivo * Number(val));
-    regressivo--;
-    return ac;
-  }, 0);
-
-  const digito = 11 - (total % 11);
-  return digito > 9 ? '0' : String(digito);
-};
-
-ValidaCPF.prototype.isSequencia = function() {
-  const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
-  return sequencia === this.cpfLimpo;
-};
-
-const cpf = new ValidaCPF('070.987.720-03');
-
-if(cpf.valida()) {
-  console.log('Cpf válido');
-} else {
-  console.log('Cpf inválido');
-}
+console.log(cpf.valida())

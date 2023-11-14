289760eab3fb4ca500047368c075b75f9a5b96df
pablo <wademcfild@gmail.com>
2023-11-14 08:00:03 -0300

    ...

---
+++ POO/47.classes/index.js
@@ -0,0 +1,9 @@
+// uma classe é um "jeito" diferente de escrever uma função construtora
+
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome  = sobrenome;
+    }
+}
+

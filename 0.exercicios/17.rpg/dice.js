a8696f27afc8342378b51a648dc7904870a6cb0f
pablo <wademcfild@gmail.com>
2023-11-16 10:10:11 -0300

    ...

--- 0.exercicios/17.rpg/dice.js
+++ 0.exercicios/17.rpg/dice.js
@@ -1,6 +1,5 @@
 function rollD20() {
-    return Math.floor(Math.random() * 20) + 1;
-  }
-  
-  var result = rollD20();
-  console.log("O resultado do seu dado d20 é: " + result);
+  return Math.floor(Math.random() * 20) + 1;
+}
+
+module.exports = rollD20;
---
+++ 0.exercicios/17.rpg/import_dice.js
@@ -0,0 +1,4 @@
+const rollD20 = require('./dice');
+
+var result = rollD20();
+console.log(result);

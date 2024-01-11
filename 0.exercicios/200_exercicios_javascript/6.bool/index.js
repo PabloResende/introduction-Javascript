cc581ec0246118c2dfd12b2d65a9c21fcdce198e
Sr.Carrilho <126430073+PabloResende@users.noreply.github.com>
2024-01-11 16:03:19 -0300

    ...

---
+++ 0.exercicios/200_exercicios_javascript/6.bool/index.js
@@ -0,0 +1,6 @@
+let bool1 = true;
+let bool2 = false;
+
+console.log("AND", bool1 && bool2)
+console.log("OR", bool1 || bool2)
+console.log("NOT", !bool1)
---
+++ 0.exercicios/200_exercicios_javascript/7.maior_menor/index.js
@@ -0,0 +1,7 @@
+let num1 = 10;
+let num2 = 20;
+let num3 = 30;
+
+console.log(num1 < num2);
+console.log(num2 > num3);
+console.log(num1 == num3);

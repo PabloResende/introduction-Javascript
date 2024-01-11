2c814ec4e706bbf3dd7fe963fbe6975f75da2b72
Sr.Carrilho <126430073+PabloResende@users.noreply.github.com>
2024-01-11 15:32:14 -0300

    ...

---
+++ 0.exercicios/20.verificar_numero/index.js
@@ -0,0 +1,9 @@
+const num = -10;
+
+if ( num > 0){
+    console.log("o número é positivo")
+} else if (num < 0) {
+    console.log("número é negativo")
+} else {
+    console.log("o número é zero")
+}
